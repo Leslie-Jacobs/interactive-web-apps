c1fe594ac66349392fc4d38f9bae82365f9be1aa
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-23 22:15:19 +0200

    changed comment to singular

--- IWA2/IWA_02-Challenge_solutions/IWA_2.5-script.js
+++ IWA2/IWA_02-Challenge_solutions/IWA_2.5-script.js
@@ -1,7 +1,10 @@
-/* (c) ACME Inc. 2010 */
+// (c) ACME Inc. 2010 
 
-/* This is the date that a user created their account */
+// This is the date that a user created their account 
 
 const date = ("10/07/2014");
 
 console.log(date);
+
+//why does the code NOT work-  output message cannot display due to missing/incomplete syntax
+//changes that was made -added parentheses,Quotes, changed type of comment used to single comment,fixed console log error
