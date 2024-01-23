833aa7848dcb8801b6eab1b264d8e412e441866b
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-23 22:54:19 +0200

    modified comment section

--- IWA2/IWA_02-Challenge_solutions/IWA_2.4-script.js
+++ IWA2/IWA_02-Challenge_solutions/IWA_2.4-script.js
@@ -9,5 +9,5 @@ const surname = ("Smith");
  console.log(user,surname);
 
 
-//why does the code NOT work-  output displays an error, as the incorrect use of semi-colon is placed.
+//why does the code NOT work-  output displays an error, as the incorrect use of semi-colon is misplaced.
 //changes that was made -added parentheses,Quotes, changed type of comment used to single comment,fixed console log error
