c0ad5a8d420ba7ce9f599cf51ea105cf54066eda
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-23 22:47:42 +0200

    changed comments

--- IWA2/IWA_02-Challenge_solutions/IWA_2.6-script.js
+++ IWA2/IWA_02-Challenge_solutions/IWA_2.6-script.js
@@ -7,6 +7,10 @@
 
 console.warn("Security scan starting");
 
-/* It is important to let user know when they can close the page*/
+//It is important to let user know when they can close the page
 
 console.info("Please wait for scan to complete before closing the browser.");
+
+
+//why does the code NOT work- incorrect spelling 
+//changes that was made - added parentheses,semi-colons, Quotes, swopped single line comments and multi-line comments.
