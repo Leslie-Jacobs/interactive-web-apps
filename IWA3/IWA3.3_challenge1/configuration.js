1184e43b5f2bf2e1ea86a335a07c3b4affa52a8e
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-24 20:10:27 +0200

    inserted export,added module type,condensed import variables

--- IWA3/IWA3.3_challenge1/configuration.js
+++ IWA3/IWA3.3_challenge1/configuration.js
@@ -3,3 +3,5 @@ const year = "2022"
 
 export {company}
 export {year}
+
+/* variables needs to be exported in order for output of the footer to display*/
--- IWA3/IWA3.3_challenge1/scripts.js
+++ IWA3/IWA3.3_challenge1/scripts.js
@@ -1,3 +1,5 @@
-import { company,year } from  "./configuration.js"
+import { company,year } from  "./configuration.js" /*fixed file path*/
 const message = ' © ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
+
+/* another way to solve is to maybe consolidate the two script files into html
