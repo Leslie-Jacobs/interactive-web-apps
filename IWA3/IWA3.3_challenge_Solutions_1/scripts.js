24a224a2049603cf77bef1765fa9442ca720ef9d
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-24 20:27:36 +0200

    added comment

--- IWA3/IWA3.3_challenge_Solutions_1/scripts.js
+++ IWA3/IWA3.3_challenge_Solutions_1/scripts.js
@@ -2,4 +2,4 @@ import { company,year } from  "./configuration.js" /*fixed file path*/
 const message = ' © ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
 
-/* another way to solve is to maybe consolidate the two script files into html*/
+
