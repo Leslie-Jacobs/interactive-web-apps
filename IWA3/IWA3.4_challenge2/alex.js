0d078f8889d1609f8e09de0691f4dd2a26e4980b
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-24 14:42:05 +0200

    added lower camel case,correctly declared variables

---
+++ IWA3/IWA3.4_challenge2/alex.js
@@ -0,0 +1,6 @@
+const firstName = "Alex";
+const surname = "Naidoo";
+const role = "Head of Marketing";
+
+const display= firstName + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
---
+++ IWA3/IWA3.4_challenge2/johannes.js
@@ -0,0 +1,6 @@
+const firstName = "Johannes";
+const surname = "Potgieter";
+const role = "Intern";
+
+const display= firstName + " " + surname + " (" + role + ")"
+document.querySelector('#johannes').innerText = display
