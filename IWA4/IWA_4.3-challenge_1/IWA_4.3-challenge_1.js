8001e454aa9cf8e87c491c670fb904dced97744e
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-30 11:42:59 +0200

    edited comment

--- IWA4/IWA_4.3-challenge_1/IWA_4.3-challenge_1.js
+++ IWA4/IWA_4.3-challenge_1/IWA_4.3-challenge_1.js
@@ -1,4 +1,4 @@
-const date = 2050  //could not define valuables as colon was implemented, (=) operator is used to assign valuables
+const date = 2050  //could not define variable as colon was implemented, (=) operator is used to assign variables
 let status = "student"
 let count = 0 
  
