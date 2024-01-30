f708527fd69f3ae15fb4294ed21ac5683d28099f
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-30 11:43:10 +0200

    added comment

--- IWA3/IWA3.3_challenge_Solutions_1/scripts.js
+++ IWA3/IWA3.3_challenge_Solutions_1/scripts.js
@@ -1,4 +1,4 @@
-import { company,year } from  "./configuration.js" /*fixed file path*/
+import { company,year } from  "./configuration.js" /*fixed file path-in order to link external correct js file*/
 const message = ' © ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
 
