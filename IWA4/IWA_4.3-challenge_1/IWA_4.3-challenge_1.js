7bd4fff4b4515255af145b2398c65ef10ff26455
leslie Jacobs <lkjlesliejade@gmail.com>
2024-01-31 16:14:56 +0200

    edited comments

--- IWA4/IWA_4.3-challenge_1/IWA_4.3-challenge_1.js
+++ IWA4/IWA_4.3-challenge_1/IWA_4.3-challenge_1.js
@@ -1,33 +1,32 @@
-const date = 2050  //could not define variable as colon was implemented, (=) operator is used to assign variables
+const date = 2050  //inserted equal operator to declare const variable date and assign it to the value of 2050
 let status = "student"
 let count = 0 
  
-if (date == 2050) {  
-	console.log("January", 'New Year’s Day')
+if (date == 2050) {   //if statement starts checking if date is equal to 2050, used comparison operator to check valuables
+	console.log("January", 'New Year’s Day') 
 	console.log("March", 'Human Rights Day')
-	let date = 'April'
 
+	let date = 'April'// new variable declared with value of april
 	console.log(date, 'Family Day')
 	console.log(date, 'Freedom Day')
 	count += 4 
 
-	if (status === "student") {
+	if (status == "student") { //if status is true, info should print out june holiday
 	  console.log('June', 'Youth Day')
 		count += 1
   }
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
-	 date = 'December'
-
+	  
+	date = 'December' /
 	console.log(date, 'Day of Reconciliation')
 	count += 3
 
-	if (status === "parent") {
+	if (status == "parent") { //if status is true, info should print out  december holidays
 	  console.log(date, 'Christmas Day')
 		count += 1
   }
-
 	console.log(date, 'Day of Goodwill')
 	count += 1
 }
