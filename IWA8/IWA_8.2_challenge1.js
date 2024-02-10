18a91ad3e6e35dc2f287166c07beddc7240832de
leslie Jacobs <lkjlesliejade@gmail.com>
2024-02-10 16:23:06 +0200

    used camelCase,nested properties,added missing colons.

--- IWA8/IWA_8.2_challenge1.js
+++ IWA8/IWA_8.2_challenge1.js
@@ -1,40 +1,48 @@
-const leoName = 'Leo Musvaire'
-const leoNumber = '2'
-const leoStreet = 'Church St.'
-const leoPostal = '3105'
-const leoBalance = '-10'
+const leoName = "Leo Musvaire";
+const leoNumber = "2";
+const leoStreet = "Church St.";
+const leoPostal = "3105";
+const leoBalance = "-10";
 
-const sarahName = 'Sarah    '
-const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.21000111'
-const sarahNumber = '13'
-const sarahStreet = 'William Close'
-const sarahPostal = '0310'
+const sarahName = "Sarah    ";
+const sarahSurname = "Kleinhans";
+const sarahBalance = "-4582.21000111";
+const sarahNumber = "13";
+const sarahStreet = "William Close";
+const sarahPostal = "0310";
 
 // Only change below this line
 
-const leo {
-	name = leoName + leoSurname
-	balance = leoBalance
-	access id = 47afb389-8014-4d0b-aff3-e40203d2107f
-	age = 24
-	address {
-		number = leoNumber
-		street = leoStreet
-		postal-code = leoPostal
-	}
+
+//leo object literal with nested address
+//Added missing colons (:) between property names and values
+//changed property names by using camelCase.
+
+const leo = {
+  name: leoName ,
+  balance: leoBalance,
+  accessId: '47afb389-8014-4d0b-aff3-e40203d2107f',
+  age: 24,
+
+  address: {
+			number: leoNumber,
+			street: leoStreet,
+			postalCode: leoPostal,
+  }
 }
-
-const sarah {
-	name = saraName + saraSurname
-	age = 62
-	access id = 6b279ae5-5657-4240-80e9-23f6b635f7a8
-	balance = sarahBalance
-	address {
-		number = sarahNumber
-		street = sarahStreet
-		postal-code = sarahPostal
-	}
-
-console.log(leo, leo[address][postal-code])
-console.log(sarah, sarah[address][postal-code])
+//sarah object literal with nested address
+const sarah = {
+  name: sarahName + ' ' + sarahSurname,
+  balance: sarahBalance,
+  accessId: '6b:57-4240-80e9-23f6b635f7a8',
+  age: 62,
+  
+   address: {
+			number: sarahNumber,
+			street: sarahStreet,
+			postalCode: sarahPostal,
+  }
+}
+//displays leo and sarah objects with postal codes
+console.log(leo,leo.address.postalCode);
+console.log(sarah,sarah.address.postalCode);
