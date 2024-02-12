b4e485d9882ad08aa545b707b2a44b48cbac2dbe
leslie Jacobs <lkjlesliejade@gmail.com>
2024-02-12 11:48:10 +0200

    added more comments

--- IWA7/IWA_7.3_challenge1.js
+++ IWA7/IWA_7.3_challenge1.js
@@ -2,8 +2,8 @@ const value = "3";
 
 //the + operator is used for both numeric addition and string concatenation.
 //the + operator is considered as a string concatenation operator rather than adding numbers,
-//this can be done using the Number() operator or parseInt() operator
+//this can be done using the Number() operator  
 
-console.log(Number(value) + 4 + Number(value));
+console.log(Number(value) + 4 + Number(value)); //logs final results
 
-console.log(typeof Number(value));
+console.log(typeof Number(value)); //displays type of operator
