ef7a93325f4610945739ab7deb3f17f8bedc108a
leslie Jacobs <lkjlesliejade@gmail.com>
2024-02-21 08:31:37 +0200

    edited syntax

--- IWA10/IWA_10-script.js
+++ IWA10/IWA_10-script.js
@@ -105,9 +105,9 @@ const lastHoliday = new Date(Math.max(...allDates));
 *converts it to a string, and pads it with '0' .
 */
 function formatDate(date) {
-    var day = date.getDate().toString().padStart(2, '0');
-    var month = (date.getMonth() + 1).toString().padStart(2, '0');
-    var year = date.getFullYear();
+    let day = date.getDate().toString().padStart(2, '0');
+    let month = (date.getMonth() + 1).toString().padStart(2, '0');
+    let year = date.getFullYear();
     return day + '/' + month + '/' + year;
 }
 
